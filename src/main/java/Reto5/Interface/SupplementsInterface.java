925a674f5fa50568c69967a076674fe3abab4e9c
SebasRomero27 <91512062+SebasRomero27@users.noreply.github.com>
2021-12-18 17:05:17 -0500

    Update SupplementsInterface.java

--- src/main/java/Reto5/Interface/SupplementsInterface.java
+++ src/main/java/Reto5/Interface/SupplementsInterface.java
@@ -3,9 +3,12 @@ package Reto5.Interface;
 import Reto5.Model.Supplements;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface SupplementsInterface extends MongoRepository<Supplements, String>
-{
+{   
+    @Query("{price:{$lte:?0}}")
     public List<Supplements> findByPrice(double price);
+    
     public List<Supplements> findByDescriptionContainingIgnoreCase(String description);   
 }
